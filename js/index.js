f67771bcb8f2bf0816a490934a090d565ce945d8
unknown <brownnguyen268@gmail.com>
2020-08-21 16:45:27 +0700

    add__2108

---
+++ js/index.js
@@ -0,0 +1,32 @@
+let menuUl = document.querySelector('.menu-ul');
+let drawer = document.querySelector('.drawer');
+let newsLi = document.querySelectorAll('.news .block-left ul li');
+let newsContentLi = document.querySelectorAll('.news .block-right ul li');
+console.log(newsContentLi)
+menuUl.onclick = function () {
+    menuUl.classList.toggle('active');
+    if (menuUl.classList.contains('active')) {
+        drawer.classList.add('active')
+    }
+    else {
+        drawer.classList.remove('active')
+    }
+}
+for (let i = 0; i < newsLi.length; i++) {
+    newsLi[i].onclick = function () {
+        switch (i) {
+            case 1: {
+                newsContentLi[1].classList.add('hide');
+                newsContentLi[2].classList.add('hide');
+                newsContentLi[0].classList.remove('hide');
+                newsContentLi[3].classList.remove('hide');
+            }
+            case 2: {
+                newsContentLi[0].classList.add('hide');
+                newsContentLi[1].classList.remove('hide');
+                newsContentLi[2].classList.add('hide');
+                newsContentLi[3].classList.remove('hide');
+            }
+        }
+    }
+}
